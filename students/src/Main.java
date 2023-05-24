a4d99b7202d86a53eb2cff74988fabb8d6d9d0e0
EBrahim Muhammed <85329448+EbrahimHeggy@users.noreply.github.com>
2023-05-24 15:18:46 +0300

    Update Main.java

--- students/src/Main.java
+++ students/src/Main.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scan  = new Scanner(System.in);
